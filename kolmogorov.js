338149ebef3acdc45b83a52f0d61e8ed5931b2c2
Samir Senhadri <sqf@o2.pl>
2016-06-04 10:00:23 +0200

    Bug fixed.

--- kolmogorov.js
+++ kolmogorov.js
@@ -1,11 +1,11 @@
 var utils = require("./utils.js");
-
 var inneMatrix = utils.getPatientsMatrix("inne.txt");
 var ang_prectMatrix = utils.getPatientsMatrix("ang_prect.txt");
 var miMatrix = utils.getPatientsMatrix("mi.txt");
 var mi_npMatrix = utils.getPatientsMatrix("mi_np.txt");
 
-var bigMatrix = utils.mergeMatrix(inneMatrix, ang_prectMatrix);
+var bigMatrix = JSON.parse(JSON.stringify(inneMatrix));
+bigMatrix = utils.mergeMatrix(bigMatrix, ang_prectMatrix);
 bigMatrix = utils.mergeMatrix(bigMatrix, miMatrix);
 bigMatrix = utils.mergeMatrix(bigMatrix, mi_npMatrix);
 
@@ -15,7 +15,11 @@ console.log(bigMatrix[1]);
 var distributionNumbers = utils.computeDistributionNumbers(bigMatrix[1]);
 
 var bigMatrixProbabilityDistribution = utils.computeProbabilityDistribution(distributionNumbers, utils.getNumberOfPatients(bigMatrix));
-console.log("bigMatrix[1].length", utils.getNumberOfPatients(bigMatrix));
+console.log("bigMatrix[1].length: ", utils.getNumberOfPatients(bigMatrix));
+console.log("inneMatrix[1].length:", utils.getNumberOfPatients(inneMatrix));
+console.log("ang_prectMatrix[1].length: ", utils.getNumberOfPatients(ang_prectMatrix));
+console.log("miMatrix[1].length: ", utils.getNumberOfPatients(miMatrix));
+console.log("mi_npMatrix[1].length: ", utils.getNumberOfPatients(mi_npMatrix));
 console.log(bigMatrixProbabilityDistribution);
 //console.log(getPatientsMatrix("mi.txt")[2][1]);
 //console.log(computeDistributionNumbers(patientsMatrix[1]));
