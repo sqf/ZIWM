6a50e78e1636e1b651a8123a457a4342c7f0875c
Samir Senhadri <sqf@o2.pl>
2016-06-04 18:44:55 +0200

    Added more calculations and refactoring.

--- kolmogorov.js
+++ kolmogorov.js
@@ -20,24 +20,60 @@ var miMatrixDistributionNumbersForAllFeatures =  utils.computeDistributionNumber
 var mi_npMatrixDistributionNumbersForAllFeatures =  utils.computeDistributionNumbersForAllFeatures(mi_npMatrix)
 var bigMatrixDistributionNumbersForAllFeatures =  utils.computeDistributionNumbersForAllFeatures(bigMatrix)
 
+var numberOfPatientsWithInne = utils.getNumberOfPatients(inneMatrix);
+var numberOfPatientsWithAng_prect = utils.getNumberOfPatients(ang_prectMatrix);
+var numberOfPatientsWithAng_prct_2 = utils.getNumberOfPatients(ang_prct_2Matrix);
+var numberOfPatientsWithMi = utils.getNumberOfPatients(miMatrix);
+var numberOfPatientsWithMi_np = utils.getNumberOfPatients(mi_npMatrix);
+var numberOfAllPatients = utils.getNumberOfPatients(bigMatrix)
+
+var probabilityOfInne = numberOfPatientsWithInne / numberOfAllPatients;
+var probabilityOfAng_prect = numberOfPatientsWithAng_prect / numberOfAllPatients;
+var probabilityOfAng_prct_2 = numberOfPatientsWithAng_prct_2 / numberOfAllPatients;
+var probabilityOfMi = numberOfPatientsWithMi / numberOfAllPatients;
+var probabilityOfMi_np = numberOfPatientsWithMi_np / numberOfAllPatients;
+
 var inneMatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(inneMatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(inneMatrix));
+    utils.computeProbabilityDistributionForAllFeatures(inneMatrixDistributionNumbersForAllFeatures, numberOfPatientsWithInne);
 var ang_prectMatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(ang_prectMatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(ang_prectMatrix));
+    utils.computeProbabilityDistributionForAllFeatures(ang_prectMatrixDistributionNumbersForAllFeatures, numberOfPatientsWithAng_prect);
 var ang_prct_2MatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(ang_prct_2MatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(ang_prct_2Matrix));
+    utils.computeProbabilityDistributionForAllFeatures(ang_prct_2MatrixDistributionNumbersForAllFeatures, numberOfPatientsWithAng_prct_2);
 var miMatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(miMatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(miMatrix));
+    utils.computeProbabilityDistributionForAllFeatures(miMatrixDistributionNumbersForAllFeatures, numberOfPatientsWithMi);
 var mi_npMatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(mi_npMatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(mi_npMatrix));
+    utils.computeProbabilityDistributionForAllFeatures(mi_npMatrixDistributionNumbersForAllFeatures, numberOfPatientsWithMi_np);
 var bigMatrixProbabilityDistributionForAllFeatures =
-    utils.computeProbabilityDistributionForAllFeatures(bigMatrixDistributionNumbersForAllFeatures, utils.getNumberOfPatients(bigMatrix));
+    utils.computeProbabilityDistributionForAllFeatures(bigMatrixDistributionNumbersForAllFeatures, numberOfAllPatients);
 
 var probabilityBigMatrixMinusInneMatrix =
     utils.subtractProbabilityDistributionForAllFeatures(bigMatrixProbabilityDistributionForAllFeatures, inneMatrixProbabilityDistributionForAllFeatures);
+var probabilityBigMatrixMinusAng_prectMatrix =
+    utils.subtractProbabilityDistributionForAllFeatures(bigMatrixProbabilityDistributionForAllFeatures, ang_prectMatrixProbabilityDistributionForAllFeatures);
+var probabilityBigMatrixMinusAng_prct_2Matrix =
+    utils.subtractProbabilityDistributionForAllFeatures(bigMatrixProbabilityDistributionForAllFeatures, ang_prct_2MatrixProbabilityDistributionForAllFeatures);
+var probabilityBigMatrixMinusMiMatrix =
+    utils.subtractProbabilityDistributionForAllFeatures(bigMatrixProbabilityDistributionForAllFeatures, miMatrixProbabilityDistributionForAllFeatures);
+var probabilityBigMatrixMinusMi_npMatrix =
+    utils.subtractProbabilityDistributionForAllFeatures(bigMatrixProbabilityDistributionForAllFeatures, mi_npMatrixProbabilityDistributionForAllFeatures);
+
 var sumOfProbabilityBigMatrixMinusInneMatrix = utils.sumProbabilitiesInEachFeature(probabilityBigMatrixMinusInneMatrix);
+var sumOfProbabilityBigMatrixMinusAng_prectMatrix = utils.sumProbabilitiesInEachFeature(probabilityBigMatrixMinusAng_prectMatrix);
+var sumOfProbabilityBigMatrixMinusAng_prct_2Matrix = utils.sumProbabilitiesInEachFeature(probabilityBigMatrixMinusAng_prct_2Matrix);
+var sumOfProbabilityBigMatrixMinusMiMatrix = utils.sumProbabilitiesInEachFeature(probabilityBigMatrixMinusMiMatrix);
+var sumOfProbabilityBigMatrixMinusMi_npMatrix = utils.sumProbabilitiesInEachFeature(probabilityBigMatrixMinusMi_npMatrix);
+
 console.log("sumOfProbabilityBigMatrixMinusInneMatrix");
 console.log(sumOfProbabilityBigMatrixMinusInneMatrix);
+console.log("sumOfProbabilityBigMatrixMinusAng_prectMatrix");
+console.log(sumOfProbabilityBigMatrixMinusAng_prectMatrix);
+console.log("sumOfProbabilityBigMatrixMinusAng_prct_2Matrix");
+console.log(sumOfProbabilityBigMatrixMinusAng_prct_2Matrix);
+console.log("sumOfProbabilityBigMatrixMinusMiMatrix");
+console.log(sumOfProbabilityBigMatrixMinusMiMatrix);
+console.log("sumOfProbabilityBigMatrixMinusMi_npMatrix");
+console.log(sumOfProbabilityBigMatrixMinusMi_npMatrix);
+
 console.log("probabilityBigMatrixMinusInneMatrix[38] :");
 console.log(probabilityBigMatrixMinusInneMatrix[38][10]);
 console.log("probabilityBigMatrixMinusInneMatrix[39] :");
@@ -45,7 +81,7 @@ console.log(probabilityBigMatrixMinusInneMatrix[39][0]);
 // console.log("inneMatrixProbabilityDistributionForAllFeatures: ");
 // console.log(inneMatrixProbabilityDistributionForAllFeatures);
 // console.log("bigMatrix[1].length: ", utils.getNumberOfPatients(bigMatrix));
-// console.log("inneMatrix[1].length:", utils.getNumberOfPatients(inneMatrix));
+console.log("utils.getNumberOfPatients(ang_prectMatrix):", utils.getNumberOfPatients(ang_prectMatrix));
 // console.log("ang_prectMatrix[1].length: ", utils.getNumberOfPatients(ang_prectMatrix));
 // console.log("miMatrix[1].length: ", utils.getNumberOfPatients(miMatrix));
 // console.log("mi_npMatrix[1].length: ", utils.getNumberOfPatients(mi_npMatrix));
